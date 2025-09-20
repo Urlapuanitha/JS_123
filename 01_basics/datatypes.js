f61c3e6bf19dac0e9466d19906e5d852a9f4c61a
Anitha Sri Urlapu <anithaani0298@gmail.com>
2025-09-20 09:53:49 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,13 @@
+//numbers 
+const num=10
+//big number 
+//string 
+let name="Ani"
+//boolean 
+let isTrue=true
+//null 
+let score=null
+//undefined 
+let newscore
+//print 
+console.log([num,isTrue,score,newscore]);
