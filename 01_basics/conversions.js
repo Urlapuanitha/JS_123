ab7fd93a443f08a379529ba9ea72c132a905302f
Anitha Sri Urlapu <anithaani0298@gmail.com>
2025-09-20 10:08:53 +0000

    container config done

---
+++ 01_basics/conversions.js
@@ -0,0 +1,22 @@
+const num=10
+let name="Ani"
+let isValid=true
+let newn=null
+let notdef
+//console.log([num,name,isValid,newn,notdef]);
+//!str to num 
+let nname=Number(name)
+//console.log(typeof nname)
+//boolean to number-yes
+let nisValid=Number(isValid)
+//console.log(nisValid);
+//null and undefined to number->0,NA
+let nnewn=Number(newn)
+let nnotdef=Number(notdef)
+//console.log([nnewn,nnotdef]);
+//number to string-yes
+let nnum=String(num);
+//console.log(typeof nnum);
+//string to boolean 
+let bname=Boolean(name)
+console.log(bname)
