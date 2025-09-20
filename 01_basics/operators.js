d4ed6779d18d54edb208add70b0d691f1330767a
Anitha Sri Urlapu <anithaani0298@gmail.com>
2025-09-20 10:20:03 +0000

    container config done

---
+++ 01_basics/operators.js
@@ -0,0 +1,18 @@
+//operators 
+let val=3
+let neg=-val
+//console.log(neg);
+//on numerical vals
+//console.log([2+2,2-2,2*2,2**2,2%2,2/2]);
+//wt on strings 
+let s1="hi"
+let s2="Ani"
+let ns=s1+s2
+//console.log(ns);
+//console.log(["1"+2,1+"2",1+1+"2","1"+1+2]);
+//console.log(+false);
+
+//comparisions
+//console.log([2<1,2>1,2==1,2>=1,2<=1]);
+///strict check ===
+console.log(["1"==1,"1"===1]);
